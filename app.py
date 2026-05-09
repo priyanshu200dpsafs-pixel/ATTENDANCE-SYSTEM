a670787b27e60e397cebff6c95471e7076a4d719
priyanshu200dpsafs-pixel <priyanshu.200dpsafs@gmail.com>
2026-05-09 08:53:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -182,4 +182,6 @@ def mark():
         return jsonify({"success": False})
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8080)
+import os
+port = int(os.environ.get("PORT", 8080))
+app.run(debug=False, host="0.0.0.0", port=port)
